15296aaca17bda42fafc989319a0b64a421031e5
Rifat Kudriakov <flungloaf@gmail.com>
2025-03-30 22:12:20 +0400

    Setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), crx({ manifest }), tailwindcss()],
   legacy: {
     skipWebSocketTokenCheck: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
